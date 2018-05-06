e922e1e20238eb0b85930c25aa2ba2a73ed4af36
jpb <jpb.06@outlook.com>
2018-05-06 20:02:42 +0200

    Setting up webpack base config and babel (babel-loader for es6 to es5)

---
+++ build/webpack.base.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, './../src/index.js'),
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+};
