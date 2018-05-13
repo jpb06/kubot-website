e26b07a77f9ce832c4cfb00606c30842c19c1758
jpb <jpb.06@outlook.com>
2018-05-13 11:33:33 +0200

    Adding css rule to webpack | Adding an alias for icons

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,58 +1,66 @@
 const webpack = require('webpack');
 const path = require('path');
 const {
-  VueLoaderPlugin
+    VueLoaderPlugin
 } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, './../src/index.ts'),
     devServer: {
-      hot: true
+        hot: true
     },
     module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-          /*options: {
-            appendTsSuffixTo: [/\.vue$/],
-          }*/
-        },
-        {
-          test: /\.vue$/,
-          use: 'vue-loader'
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'vue-style-loader',
-            'css-loader',
-            'sass-loader'
-          ]
-        }
-      ]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                /*options: {
+                  appendTsSuffixTo: [/\.vue$/],
+                }*/
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     },
     resolve: {
-      alias: {
-        vue$: 'vue/dist/vue.esm.js'
-      }
+        alias: {
+            vue$: 'vue/dist/vue.esm.js',
+            '@icons': path.resolve(__dirname, '..', 'node_modules/vue-material-design-icons')
+        }
     },
     plugins: [
-      new VueLoaderPlugin(),
-      new webpack.HotModuleReplacementPlugin(),
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'index.html',
-        inject: true
-      }),
+        new VueLoaderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
     ]
 };
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,53 +1,61 @@
 const path = require('path');
 const {
-  VueLoaderPlugin
+    VueLoaderPlugin
 } = require('vue-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, './../src/index.ts'),
     module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-          /*options: {
-            appendTsSuffixTo: [/\.vue$/],
-          }*/
-        },
-        {
-          test: /\.vue$/,
-          use: 'vue-loader'
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader"
-          }
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'vue-style-loader',
-            'css-loader',
-            'sass-loader'
-          ]
-        }
-      ]
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                /*options: {
+                  appendTsSuffixTo: [/\.vue$/],
+                }*/
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     },
     resolve: {
-      alias: {
-        vue$: 'vue/dist/vue.esm.js'
-      }
+        alias: {
+            vue$: 'vue/dist/vue.esm.js',
+            '@icons': path.resolve(__dirname, '..', 'node_modules/vue-material-design-icons')
+        }
     },
     plugins: [
-      new VueLoaderPlugin(),
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: 'index.html',
-        inject: true
-      }),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        }),
     ]
 };
